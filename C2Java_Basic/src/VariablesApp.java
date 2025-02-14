97c5d067ca2e9b8bf3b379502fe7bfdeaae22455
AniBnP <NIGHJHUREY17@gmail.com>
2025-02-14 11:04:47 +0100

    Pruebas de variables y constantes

---
+++ C2Java_Basic/src/VariablesApp.java
@@ -0,0 +1,13 @@
+
+public class VariablesApp {
+
+	public static void main(String[] args) {
+		int a = 30; // Declaro el tipo y el nombre de ña variable y asigno un valor 30
+		double b = 30.5; // Declaro el tipo, nombre y asigno valor
+		a = 50; 
+// int a =32; No puedes porque se esta declarando de nuevo y ya existe
+		double suma = a + b;
+		System.out.println(suma);
+	}
+
+}
