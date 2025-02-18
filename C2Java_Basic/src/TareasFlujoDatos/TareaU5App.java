3f52bb42c2a7d8e71cab8588df9618870d2ccc39
AniBnP <NIGHJHUREY17@gmail.com>
2025-02-18 13:24:00 +0100

    TareaUD5

--- C2Java_Basic/src/TareasFlujoDatos/TareaU5App.java
+++ C2Java_Basic/src/TareasFlujoDatos/TareaU5App.java
@@ -3,15 +3,24 @@ package TareasFlujoDatos;
 public class TareaU5App {
 
 	public static void main(String[] args) {
+		// Declaracion de variables
 		int op = 12 ; 
 		int op2 = 69 ;
 		
-		//Comparacion de los numeros
+		// Se utiliza una estructura condicional (if-else) para comparar los valores de las dos variables
+		
+		// SI el valor de "op" es mayor que el valor op2
 		if (op > op2) {
+			// si op es mayor, imprime que op es mayor que op2
 			System.out.println(op + " es mayor que " + op2);
-		} else if (op < op2) {
+		} 
+		// si op no es mayor que op2, entonces verifica si op es menor que op2
+		else if (op < op2) {
 			System.out.println(op2 + " es mayor que " + op);
-		} else {
+		} 
+		//Si ninguna de las condiciones anteriores es verdadera, significa que los dos numeros son iguales
+		else {
+			//Imprime que los dos numeros son iguales
 			System.out.println(op + " y " + op2 + "son iguales");
 		}
 
