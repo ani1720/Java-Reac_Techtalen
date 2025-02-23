a82cbd7a9ff472cbc390f19a3913c87ac6d4a295
AniBnP <NIGHJHUREY17@gmail.com>
2025-02-23 17:36:18 +0100

    "Añadido nuevo archivo"

---
+++ C2Java_Basic/src/UD05FlujoDatos/Ejercicios/Tarea06UD5App.java
@@ -0,0 +1,32 @@
+package UD05FlujoDatos.Ejercicios;
+
+import java.util.Scanner;
+
+public class Tarea06UD5App {
+
+	public static void main(String[] args) {
+		 // Crear una instancia de Scanner para leer la entrada del usuario
+        Scanner scanner = new Scanner(System.in);
+
+        // Solicitar al usuario que ingrese un número
+        System.out.println("Por favor, ingresa un número:");
+
+        // Leer el número ingresado por el usuario
+        int numero = scanner.nextInt();
+
+        // Verificar si el número es divisible entre 2
+        if (esDivisiblePorDos(numero)) {
+            System.out.println("El número " + numero + " es divisible entre 2.");
+        } else {
+            System.out.println("El número " + numero + " no es divisible entre 2.");
+        }
+
+        // Cerrar el scanner
+        scanner.close();
+    }
+
+    // Método booleano para verificar si un número es divisible entre 2
+    public static boolean esDivisiblePorDos(int numero) {
+        return numero % 2 == 0;
+    }
+}
