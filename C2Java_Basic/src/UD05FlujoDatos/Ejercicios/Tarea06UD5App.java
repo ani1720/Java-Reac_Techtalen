7cdb2561fabf30890cb90215e2695e0421011e02
AniBnP <NIGHJHUREY17@gmail.com>
2025-02-24 12:03:27 +0100

    Create Tarea06UD5App.java

---
+++ C2Java_Basic/src/UD05FlujoDatos/Ejercicios/Tarea06UD5App.java
@@ -0,0 +1,17 @@
+package UD05FlujoDatos.Ejercicios;
+
+import javax.swing.JOptionPane;
+
+public class Tarea06UD5App {
+
+	public static void main(String[] args) {
+
+		final double IVA = 0.21;
+		String precioProducto = JOptionPane.showInputDialog (null, "Ingrese el precio del producto ") ;
+		
+		double precio = Integer.parseInt(precioProducto) ;
+		JOptionPane.showMessageDialog(null, "El producto cuesta " +precio+ "Prodcuto con IVA "+ (precio*IVA));
+		
+	}
+
+}
