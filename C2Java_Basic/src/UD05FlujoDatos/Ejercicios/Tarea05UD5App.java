ca28578c69b21520ae7fda9f5bbd21faf1f2e908
AniBnP <NIGHJHUREY17@gmail.com>
2025-02-24 11:18:32 +0100

    Update Tarea05UD5App.java

--- C2Java_Basic/src/UD05FlujoDatos/Ejercicios/Tarea05UD5App.java
+++ C2Java_Basic/src/UD05FlujoDatos/Ejercicios/Tarea05UD5App.java
@@ -4,19 +4,18 @@ import java.util.Scanner;
 
 public class Tarea05UD5App {
 
-	public static boolean main(String[] args) {
-		 // Crear una instancia de Scanner para leer la entrada del usuario
-        Scanner scanner = new Scanner(System.in);
+	public static void main(String[] args) {
+		// Crear una instancia de Scanner para leer la entrada del usuario
+		Scanner scanner = new Scanner(System.in);
 
-        // Solicitar al usuario que ingrese un número
-        System.out.println("Por favor, ingresa un número:");
+		// Solicitar al usuario que ingrese un número
+		System.out.println("Por favor, ingresa un número:");
 
-        // Leer el número ingresado por el usuario
-        int numero = scanner.nextInt();
+		// Leer el número ingresado por el usuario
+		int numero = scanner.nextInt();
 
-        // Cerrar el scanner
-        scanner.close();
-    }
+		// Cerrar el scanner
+		scanner.close();
+	}
 
- 
-    }
+}
