24a392a7111ea9f803870ef7ebf4921cde7fcb73
AniBnP <NIGHJHUREY17@gmail.com>
2025-02-24 19:56:17 +0100

    Update Tarea08UD5App.java

--- C2Java_Basic/src/UD05FlujoDatos/Ejercicios/Tarea08UD5App.java
+++ C2Java_Basic/src/UD05FlujoDatos/Ejercicios/Tarea08UD5App.java
@@ -4,10 +4,9 @@ public class Tarea08UD5App {
 
 	public static void main(String[] args) {
 		int a = 24 ;
-		for (int a = 0; a >20; a++) ;
+		for (int a = 24; a >20; a++) ;
 			System.out.println();
 		}
 
 	}
 
-}
