d9f44d3353dde9be3947fe9cab4ad9b9b7b031d8
AniBnP <NIGHJHUREY17@gmail.com>
2025-02-24 13:59:33 +0100

    ejercicios

---
+++ C2Java_Basic/src/UD05FlujoDatos/Ejercicios/Tarea07UD5App.java
@@ -0,0 +1,14 @@
+package UD05FlujoDatos.Ejercicios;
+
+public class Tarea07UD5App {
+
+	public static void main(String[] args) {
+		int num = 101;
+		while (num <= 100) {
+			System.out.println(num);
+			num++;
+		}
+
+	}
+
+}
---
+++ C2Java_Basic/src/UD05FlujoDatos/Ejercicios/Tarea08UD5App.java
@@ -0,0 +1,13 @@
+package UD05FlujoDatos.Ejercicios;
+
+public class Tarea08UD5App {
+
+	public static void main(String[] args) {
+		int a = 24 ;
+		for (int a = 0; a >20; a++) ;
+			System.out.println();
+		}
+
+	}
+
+}
