f20f18ecffb328675b002a3363db0c5c3d33806a
AniBnP <NIGHJHUREY17@gmail.com>
2025-02-25 11:31:07 +0100

    Update Tarea09UD5bApp.java

--- C2Java_Basic/src/UD05FlujoDatos/Ejercicios/Tarea09UD5bApp.java
+++ C2Java_Basic/src/UD05FlujoDatos/Ejercicios/Tarea09UD5bApp.java
@@ -1,7 +1,7 @@
 package UD05FlujoDatos.Ejercicios;
 import java.util.Scanner;
 
-public class Tarea09bUD5App {
+public class Tarea09UD5bApp {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
