4f73719eaa69e4ef4665ad5076eb07713e5f7f6f
AniBnP <NIGHJHUREY17@gmail.com>
2025-03-10 14:02:08 +0100

    trabajo equipo

--- C2Java_Basic/src/UD06Ejercicios/Tarea02UD6App.java
+++ C2Java_Basic/src/UD06Ejercicios/Tarea02UD6App.java
@@ -1,10 +1,31 @@
 package UD06Ejercicios;
 
+import java.util.Random;
+
+import javax.swing.JOptionPane;
+
 public class Tarea02UD6App {
 
 	public static void main(String[] args) {
-		//System.out.println(Math.random()*100);
-		
+		// System.out.println(Math.random()*100);
+		int numIngresado = Integer.parseInt(JOptionPane.showInputDialog("Introduce un numero "));
+
+		int numMaxi = Integer.parseInt(JOptionPane.showInputDialog("introduce el rango maximo "));
+
+		int numMini = Integer.parseInt(JOptionPane.showInputDialog("Introduce el rango minimo "));
+			if (numMini <= 0) {
+		JOptionPane.showMessageDialog(null, "No puede ser 0");
+		return;	}
+			
+			}
+			
+				//System.out.println(valorRandom(numMaxi,numMini));
+
+
+	public static int valorRandom(int numMaxi, int numMini) {
+		Random random = new Random();
+		 int result = random.nextInt(numMaxi-numMini+1) + numMini ;
+		 return result; 
 	}
 
 }
--- C2Java_Basic/src/UD06Ejercicios/Tarea12UD06App.java
+++ C2Java_Basic/src/UD06Ejercicios/Tarea12UD06App.java
@@ -1,5 +1,6 @@
 package UD06Ejercicios;
 
+import java.util.Arrays;
 import java.util.Random;
 
 import javax.swing.JOptionPane;
@@ -7,34 +8,63 @@ import javax.swing.JOptionPane;
 public class Tarea12UD06App {
 
 	public static void main(String[] args) {
-		int dimension = Integer.parseInt(JOptionPane.showInputDialog("Introduce la dimensión de los arrays: "));
-       
-		int[] array1 = new int [dimension] ;
- 
-		array1 = randomArray1(array1, dimension) ;
-		
-		int numero = Integer.parseInt(JOptionPane.showInputDialog("Introduce el ultimo valor que quieres"));
+		int dimension = Integer.parseInt(JOptionPane.showInputDialog("Introduce la dimensión del array: "));
+		// Pedimos la dimensión del array
+		int[] array1 = new int[dimension];
+		// Creamos el array con la dimensión
+
+		System.out.println("El array creado random es: " + Arrays.toString(randomArray1(array1)));
+		// Mostramos el array creado por el random
+
+		// Pedimos el último valor que se quiere mostrar
+		int numero = Integer.parseInt(JOptionPane.showInputDialog("Introduce el último valor que quieres: "));
+
+		System.out.println("El nuevo array creado con el valor final " + numero + " es: "
+				+ Arrays.toString(comparador(array1, numero)));
+// Imprimimos el nuevo array que contiene todos los valores random pero que acaban con el número introducido
+
 	}
-	public static int[] randomArray1(int[] array1, int dimension) {
-		 // Metodo de donde llamaremos al array
-		array1 = new int[dimension] ;
-		
-		if (dimension <= 0) {
-			 JOptionPane.showMessageDialog(null,"La dimension introducida no es valida"); 
+	public static int[] randomArray1(int[] array1) {
+
+		// Crear un método que haga que los arrays sean creados random
+		if (array1.length <= 0) {
+			JOptionPane.showMessageDialog(null, "La dimensión del array no es válida.");
+
 		} else {
-	        for (int i = 0; i < dimension; i++) {
-	            array1[i] = (int) (Math.random() * 300) + 1;
-	        }
+
+			for (int i = 0; i < array1.length; i++) {
+				array1[i] = (int) (Math.random() * 300) + 1;
+			}
 		}
-	        return array1; 
+		return array1;
 	}
-public static int comparador (int[] array1, int numero) {
-	int numero = 0 ;
-	for (int resultados : array1) {
-		if (resultados % 10 == numero) {
-			numero++;
+
+	public static int[] comparador(int[] array1, int numero) {
+		// Contar cuántos números terminan en el dígito especificado
+		int count = 0;
+		for (int i = 0; i < array1.length; i++) {
+			if (array1[i] % 10 == numero) {
+				count++;
+			}
 		}
+
+		// Crear un nuevo array para almacenar los números que terminan en el dígito
+		// especificado
+		int[] arrayFinal = new int[count];
+		int posicion = 0;
+		for (int i = 0; i < array1.length; i++) {
+			if (array1[i] % 10 == numero) {
+				arrayFinal[posicion++] = array1[i];
+			}
+		}
+
+		// Imprimir un mensaje si no se encontraron números que terminan en el dígito
+		// especificado
+		if (count == 0) {
+			System.out.println("No hay ningún número con el valor " + numero);
+		}
+
+		return arrayFinal;
 	}
-}
 }
 	
