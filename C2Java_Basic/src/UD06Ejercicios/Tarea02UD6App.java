2282068c6cfd797bc41de60c09f546750e81ea68
AniBnP <NIGHJHUREY17@gmail.com>
2025-03-11 19:24:54 +0100

    ejercicios

--- C2Java_Basic/src/UD06Ejercicios/Tarea02UD6App.java
+++ C2Java_Basic/src/UD06Ejercicios/Tarea02UD6App.java
@@ -8,24 +8,28 @@ public class Tarea02UD6App {
 
 	public static void main(String[] args) {
 		// System.out.println(Math.random()*100);
-		int numIngresado = Integer.parseInt(JOptionPane.showInputDialog("Introduce un numero "));
+		int numRandoms = Integer.parseInt(JOptionPane.showInputDialog("Introduce un numero "));
+		int numMaxi;
+		int numMini ;
+	if (numRandoms <= 0) {
+JOptionPane.showMessageDialog(null, "No puede ser 0");
+return; }
+			for (int i = numRandoms; i>0; i++) {
+				numMaxi = Integer.parseInt(JOptionPane.showInputDialog("introduce el rango maximo "));
 
-		int numMaxi = Integer.parseInt(JOptionPane.showInputDialog("introduce el rango maximo "));
-
-		int numMini = Integer.parseInt(JOptionPane.showInputDialog("Introduce el rango minimo "));
-			if (numMini <= 0) {
-		JOptionPane.showMessageDialog(null, "No puede ser 0");
-		return;	}
-			
+				numMini = Integer.parseInt(JOptionPane.showInputDialog("Introduce el rango minimo "));
+			if (numMini>=numMaxi) {
+				JOptionPane.showMessageDialog(null, "El rango minimo debe ser mayor que el rango maximo");
 			}
+			int randomValor= valorRandom(numMaxi, numMini);
+			JOptionPane.showMessageDialog(null, "Numero aleatorio generado: " + randomValor);
+			}
+		} 
 			
-				//System.out.println(valorRandom(numMaxi,numMini));
-
-
 	public static int valorRandom(int numMaxi, int numMini) {
 		Random random = new Random();
-		 int result = random.nextInt(numMaxi-numMini+1) + numMini ;
-		 return result; 
+		int result = random.nextInt(numMaxi - numMini + 1) + numMini;
+		return result;
 	}
 
 }
---
+++ C2Java_Basic/src/UD06Ejercicios/Tarea03UD06App.java
@@ -0,0 +1,19 @@
+package UD06Ejercicios;
+
+import javax.swing.JOptionPane;
+
+public class Tarea03UD06App {
+
+	public static void main(String[] args) {
+		//Creamos el entero 
+	int numPrimo= 13;
+	//Hacemos la conversion 
+	 numPrimo= Integer.parseInt(JOptionPane.showInputDialog("Introduce un numero primo: ")) ;
+
+	}
+public boolean primoSi (int valorPrimo) {
+	
+		
+	}
+}
+}
