ed5e3912a7ad3cde6891a58efc35bb9ddd5f6a22
AniBnP <NIGHJHUREY17@gmail.com>
2025-03-12 13:52:36 +0100

    tareas

--- C2Java_Basic/src/UD06Ejercicios/Tarea02UD6App.java
+++ C2Java_Basic/src/UD06Ejercicios/Tarea02UD6App.java
@@ -7,20 +7,34 @@ import javax.swing.JOptionPane;
 public class Tarea02UD6App {
 
 	public static void main(String[] args) {
-		// System.out.println(Math.random()*100);
+		// System.out.println(Math.random()*100); operacion de aritmetica para hacer random
+		// asignamos los enteros donde uno lo introduce el usuario
 		int numRandoms = Integer.parseInt(JOptionPane.showInputDialog("Introduce un numero "));
 		int numMaxi;
 		int numMini ;
+		/* Hacemos un bucle donde dice si el numero introducido es menor o igual a 0 
+		 * donde diga al usuario que el numero introducido no puede ser 0 
+		 */
 	if (numRandoms <= 0) {
 JOptionPane.showMessageDialog(null, "No puede ser 0");
 return; }
+	/* creamos un bucle FOR donde le asignamos que el entero i es igual a numero random 
+	 * ingresado por el usuario
+	 * decimos que i es mayor a 0
+	 * y que i se incremente
+	 */
 			for (int i = numRandoms; i>0; i++) {
 				numMaxi = Integer.parseInt(JOptionPane.showInputDialog("introduce el rango maximo "));
 
 				numMini = Integer.parseInt(JOptionPane.showInputDialog("Introduce el rango minimo "));
-			if (numMini>=numMaxi) {
-				JOptionPane.showMessageDialog(null, "El rango minimo debe ser mayor que el rango maximo");
+		
+		/* Luego haremos un IF donde decimos que SI el minimo es mayor o igual al mayor
+		 * le diremos al usuario que debe ingresar un rango menor al maximo
+		 */
+				if (numMini>=numMaxi) {
+				JOptionPane.showMessageDialog(null, "El rango minimo debe ser menor que el rango maximo");
 			}
+				// declaramos un entero donde se llama al metodo hecho para el valor random
 			int randomValor= valorRandom(numMaxi, numMini);
 			JOptionPane.showMessageDialog(null, "Numero aleatorio generado: " + randomValor);
 			}
--- C2Java_Basic/src/UD06Ejercicios/Tarea03UD06App.java
+++ C2Java_Basic/src/UD06Ejercicios/Tarea03UD06App.java
@@ -5,15 +5,39 @@ import javax.swing.JOptionPane;
 public class Tarea03UD06App {
 
 	public static void main(String[] args) {
-		//Creamos el entero 
-	int numPrimo= 13;
-	//Hacemos la conversion 
-	 numPrimo= Integer.parseInt(JOptionPane.showInputDialog("Introduce un numero primo: ")) ;
+		// Creamos el entero que no es primo
+		int numPrimo = 0;
+		// Hacemos la conversion con parseInt para introducir un string y pase a entero
+		numPrimo = Integer.parseInt(JOptionPane.showInputDialog("Introduce un numero primo: "));
+		// usamos un bucle if donde le decimos que si el numero inrresado es menor o igual 1 
+		if (numPrimo <= 1) {
+			JOptionPane.showMessageDialog(null, String.format("El %d es primo es <=1", numPrimo));
+			//decimos que si no es menor o igual a 1. Entonces es un numero primo
+		}  else if (esPrimo(numPrimo)) {
+			JOptionPane.showMessageDialog(null, String.format("El %d es primo", numPrimo));
+			// En caso que no cumpla ninguna de las 2 anteriores decimos que no es primo 
+			//	y le damos fin al bucle
+		}else {
+			JOptionPane.showMessageDialog(null, String.format("El %d no es primo ", numPrimo));
+		}
 
 	}
-public boolean primoSi (int valorPrimo) {
-	
-		
+
+	public static boolean esPrimo(int valorPrimo) {
+		// Verificamos mediante un bucle SI el valor es menor o igual a 1 nos devuelve FALSE
+		if (valorPrimo <=1) {
+			return false;
+		}
+/* Utilizamos el bucle for para comprobar si el numero tiene divisores distintos
+ * a 1 y de el mismo. Entonces recorrera desde 2 hasta la raiz cuadrada del numero
+ * Asi que si encuentra divisor donde de 0 devuelve FALSE y sino es munero primo y devuelve TRUE
+**/
+		for (int i = 2; i <= Math.sqrt(valorPrimo); i++) {
+			if (valorPrimo % i == 0) {
+				return false;
+			}
+		}
+		return true;
+
 	}
-}
 }
