0721f8731ad570591e12755dbc7d69f6771ac588
AniBnP <NIGHJHUREY17@gmail.com>
2025-03-13 17:37:01 +0100

    Update Tarea05UD06App.java

--- C2Java_Basic/src/UD06Ejercicios/Tarea05UD06App.java
+++ C2Java_Basic/src/UD06Ejercicios/Tarea05UD06App.java
@@ -1,10 +1,22 @@
 package UD06Ejercicios;
 
+import javax.swing.JOptionPane;
+
 public class Tarea05UD06App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		double numero = 0.0;
+		numero = Double.parseDouble(JOptionPane.showInputDialog("Ingrese el decimal"));
+		
 	}
-
+public double binario(int divisor) {
+	String resultado="";
+	int resto;
+	do {
+		resto = divisor % 2;
+		divisor = resto / 2;
+		resultado =resto + resultado ;
+		
+	}while (divisor > 0);
+}
 }
