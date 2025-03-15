bcbae5d936693e769b16fc356722befd24933867
AniBnP <NIGHJHUREY17@gmail.com>
2025-03-15 15:51:04 +0100

    Update Tarea07UD06App.java

--- C2Java_Basic/src/UD06Ejercicios/Tarea07UD06App.java
+++ C2Java_Basic/src/UD06Ejercicios/Tarea07UD06App.java
@@ -1,10 +1,29 @@
 package UD06Ejercicios;
 
+import javax.swing.JOptionPane;
+
 public class Tarea07UD06App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+		double euros;
+		
+		euros = JOptionPane.showConfirmDialog(null, "Introduce a que moneda quieres hacer la conversion");
+		
 	}
-
+public static double dolar (double valor) {
+	//valor = Double.parseDouble(JOptionPane.showInputDialog("Introduce el valor en EURO(€)"));
+	final double usd = 1.09;
+	return valor * usd ;
+}
+public static double libra (double valor) {
+	final double libr = 0.86 ;
+	return valor * libr ;
+	
+}
+public static double yenes (double valor) {
+	final double yen = 129.852 ;
+	return valor * yen ;
+	
+}
 }
