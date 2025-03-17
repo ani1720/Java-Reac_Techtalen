1395ab712c73d245aef8720caf861e8102350b4d
AniBnP <NIGHJHUREY17@gmail.com>
2025-03-17 18:38:37 +0100

    correccion

--- C2Java_Basic/src/UD06Ejercicios/Tarea07UD06App.java
+++ C2Java_Basic/src/UD06Ejercicios/Tarea07UD06App.java
@@ -5,61 +5,68 @@ import javax.swing.JOptionPane;
 public class Tarea07UD06App {
 
 	public static void main(String[] args) {
-		// Inicializamos las variables 
-	String moneda = "";
+		// Inicializamos las variables
+		String moneda = "";
 		double euros = 0.0; // asignamos valor 0 para poder ejecutar
 		String input = JOptionPane.showInputDialog(null, "Ingresa un valor en EURO (€)");
-		//Aplicamos un Try Catch para evitar un error al introducir un numero o algun error
+		// Aplicamos un Try Catch para evitar un error al introducir un numero o algun
+		// error
 		try {
 			euros = Double.parseDouble(input);
-			JOptionPane.showMessageDialog(null, "Has introducido: "+ euros + " euro");
+			JOptionPane.showMessageDialog(null, "Has introducido: " + euros + " euro");
 		} catch (NumberFormatException e) {
-			JOptionPane.showMessageDialog(null, "Por favor, introduce un numero valido.", "Error", JOptionPane.ERROR_MESSAGE);
+			JOptionPane.showMessageDialog(null, "Por favor, introduce un numero valido.", "Error",
+					JOptionPane.ERROR_MESSAGE);
 		}
-		// realizamos un bucle en el que utilizamos un EQUALSIGNORECASE para evitar errores en las cadenas
+		// realizamos un bucle en el que utilizamos un EQUALSIGNORECASE para evitar
+		// errores en las cadenas
 		moneda = JOptionPane.showInputDialog(null, "Introduce el tipo de moneda que quieres cambiar ");
-		if (moneda.equalsIgnoreCase("dolar")) {
-			
+		/*if (moneda.equalsIgnoreCase("dolar")) {
+
 		} else if (moneda.equalsIgnoreCase("libras")) {
-			
+
 		} else if (moneda.equalsIgnoreCase("yenn")) {
-			
+
 		} else {
 			JOptionPane.showMessageDialog(null, "Divisa no reconocida");
+		}*/
+		double result = 0.0;
+		switch (moneda.toLowerCase()) {
+		case "dolar":
+			result = convertirDolar(euros);
+			JOptionPane.showMessageDialog(null, "Seleccionaste Dolar");
+			break;
+		case "libras":
+			result = convertirLibra(euros);
+			JOptionPane.showMessageDialog(null, "Has seleccionado Libras");
+			break;
+		case "yenn":
+			result = convertirYenes(euros);
+			JOptionPane.showMessageDialog(null, "Seleccionaste yenes");
+			break;
+		default:
+			JOptionPane.showMessageDialog(null, "Moneda no reconocida", "Error", JOptionPane.ERROR_MESSAGE);
+			System.exit(0);
 		}
-		double result = 0.0 ;
-switch (moneda.toLowerCase()) {
-case "dolar" : 
-	result = convertirDolar(euros);
-	JOptionPane.showMessageDialog(null, "Seleccionaste Dolar");
-	break;
-case "libras" : 
-	result = convertirLibra(euros);
-	JOptionPane.showMessageDialog(null, "Has seleccionado Libras");
-	break;
-case "yenn" :
-	result = convertirYenes(euros);
-	JOptionPane.showMessageDialog(null, "Seleccionaste yenes");
-	break;
-default :
-		JOptionPane.showMessageDialog(null, "Moneda no reconocida", "Error", JOptionPane.ERROR_MESSAGE);
-	System.exit(0);
-}
-JOptionPane.showMessageDialog(null, "El valor convertido es: "+ result);
+		JOptionPane.showMessageDialog(null, "El valor convertido es: " + result);
 	}
-public static double convertirDolar (double valor) {
-	//valor = Double.parseDouble(JOptionPane.showInputDialog("Introduce el valor en EURO(€)"));
-	final double usd = 1.09;
-	return valor * usd ;
-}
-public static double convertirLibra (double valor) {
-	final double libr = 0.86 ;
-	return valor * libr ;
-	
-}
-public static double convertirYenes (double valor) {
-	final double yen = 129.852 ;
-	return valor * yen ;
-	
-}
+
+	public static double convertirDolar(double valor) {
+		// valor = Double.parseDouble(JOptionPane.showInputDialog("Introduce el valor en
+		// EURO(€)"));
+		double usd = 1.09;
+		return valor * usd;
+	}
+
+	public static double convertirLibra(double valor) {
+		double libr = 0.86;
+		return valor * libr;
+
+	}
+
+	public static double convertirYenes(double valor) {
+		double yen = 129.852;
+		return valor * yen;
+
+	}
 }
--- C2Java_Basic/src/UD06Ejercicios/Tarea08UD06App.java
+++ C2Java_Basic/src/UD06Ejercicios/Tarea08UD06App.java
@@ -7,13 +7,18 @@ import javax.swing.JOptionPane;
 public class Tarea08UD06App {
 
 	public static void main(String[] args) {
-		int array;		
+		int[] array= new array [10];
+		JOptionPane.showInputDialog("Introduce un los valores a asignar");
+		
 	}
+	public
+	/* posible metodo
 public static int[] valores () {
 	int[] valor = new valor[10]; 
 			int relleno = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese los valores"));
 	for (int i = 0; i < relleno; i++) {
 		array[i] = relleno.nextInt(); 
 	}
+	*/
 }
-}
+
