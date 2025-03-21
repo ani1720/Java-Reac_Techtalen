b9373c0544b3b0e8dd90d8e48cbac6d32e78f1c8
AniBnP <NIGHJHUREY17@gmail.com>
2025-03-21 12:50:03 +0100

    tarea que un no entiendo

--- C2Java_Basic/src/UD06Ejercicios/Tarea10UD06App.java
+++ C2Java_Basic/src/UD06Ejercicios/Tarea10UD06App.java
@@ -1,51 +1,77 @@
 package UD06Ejercicios;
 
 import javax.swing.JOptionPane;
-
+import java.util.Arrays;
 public class Tarea10UD06App {
 
 	public static void main(String[] args) {
-		int[] numeros = new int [dimensionArray()] ;
-		
+		int[] numeros = new int[dimensionArray()];
+
 		int maximo = 0;
-		maximo= Integer.parseInt(JOptionPane.showInputDialog("Introduce el valor maximo del Array"));
-		
+		maximo = Integer.parseInt(JOptionPane.showInputDialog("Introduce el valor maximo del Array"));
+
 		int minimo = 0;
 		minimo = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el valor minimo del Array"));
+	
+	valorRandomPrimos(numeros, minimo, maximo);
+	
+		for (int i = 0; i < numeros.length; i++) {
+			if (i==0) {
+				JOptionPane.showMessageDialog(null, "Los numeros primos son: {[" + i + "] ="+ numeros[i] + ", ");
+			} else if (i == numeros.length - 1) {
+				JOptionPane.showMessageDialog(null, "[" + i + "] = "+numeros[i] + "}");
+			}else if (i == numeros.length - 2) {
+				JOptionPane.showMessageDialog(null, "[" + i + "] ="+numeros[i] + ",");
+			}else {
+				JOptionPane.showMessageDialog(null, "[" + i + "] ="+numeros[i]);
+			}
+		}
 	}
-private static int dimensionArray () { //MEtodo para ingresar el tamaño del array
-	int dimension = 0;
-	dimension= Integer.parseInt(JOptionPane.showInputDialog("Ingrese el tamaño del que quieres el Array")) ;
-	return dimension;
-}
-private static int numPrimo (int min, int maxi) { //En este metodo
-	int num = 0;
-	boolean esPrimo = false;
-	if (!esPrimo) {
-		num = (int) (Math.random() * (maxi - min + 1) + min);
-		esPrimo = esPrimo(num);
+
+	private static int dimensionArray() { // MEtodo para ingresar el tamaño del array
+		int dimension = 0;
+		dimension = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el tamaño del que quieres el Array"));
+		return dimension;
 	}
-	return num;
-}
-private static void valorRandomPrimos (int[] numeroPrimo, int min, int max) {
-	for (int i = 0 ; i <= numeroPrimo.length ; i++) {
-		numeroPrimo[i] = numPrimo(min, max) ;
-		
+
+	private static int numPrimo(int minimo, int maximo) { // En este metodo sacaremos el numero primo
+		int num = 0;
+		boolean esPrimo = false;
+		if (!esPrimo) {
+			num = (int) (Math.random() * (maximo - minimo + 1) + minimo);
+			esPrimo = esPrimo(num);
+		}
+		return num;
 	}
-}
-public static boolean esPrimo(int valorPrimo) {
-	boolean primoSiNo = true ;
-	if (valorPrimo <= 1) {
-		primoSiNo = false;
-		return primoSiNo;
+
+	private static void valorRandomPrimos(int[] numeros, int minimo, int maximo) {
+		for (int i = 0; i <= numeros.length; i++) {
+			numeros[i] = numPrimo(minimo, maximo);
+
+		}
 	}
-	for (int i = 2; i <=Math.sqrt(valorPrimo); i++) {
-		if (valorPrimo % i ==0) {
+
+	public static boolean esPrimo(int valorPrimo) {
+		boolean primoSiNo = true;
+		if (valorPrimo <= 1) {
 			primoSiNo = false;
 			return primoSiNo;
 		}
+		for (int i = 2; i <= Math.sqrt(valorPrimo); i++) {
+			if (valorPrimo % i == 0) {
+				primoSiNo = false;
+				return primoSiNo;
+			}
+		}
+		return primoSiNo;
 	}
-	return primoSiNo;
+private static int buscarMayor (int[] numeros) {
+	int mayor = 0;
+	for (int i = 0; i < numeros.length; i++) {
+		if (numeros[i] > mayor) {
+			mayor = numeros[i] ;
+		}
+	}
+	return mayor;
 }
-
 }
