b74137c44b0a98553b8fd06cf78b8d245b225e78
AniBnP <NIGHJHUREY17@gmail.com>
2025-04-17 09:19:13 +0200

    Mejoras T6

--- C2Java_Basic/src/UD09Ejercicios/Tarea06/Tarea06UD09App.java
+++ C2Java_Basic/src/UD09Ejercicios/Tarea06/Tarea06UD09App.java
@@ -11,7 +11,7 @@ public class Tarea06UD09App {
 				"Selecciona una Opcion", JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, pelicula,
 				pelicula[0]);
 
-	//	SubPeliculasUD09 peliSelec = null;
+		SubPeliculasUD09 peliSelec = null;
 		switch (eleccion) {
 		case 0:
 			SubPeliculasUD09 peli1 = new SubPeliculasUD09("Super Mario Bros. La película", 12.99, 92, 7,
@@ -19,7 +19,7 @@ public class Tarea06UD09App {
 			JOptionPane.showMessageDialog(null, "Has seleccionado: " + "Super Mario Bros.\n Dura "
 					+ peli1.getDuracionPeli() + " minutos y es para mayores de " + peli1.getEdadMinima() + " años.");
 			peli1.comprarEntrada(peli1.getEdadMinima(), peli1.precioEntrada);
-
+			peliSelec = peli1;
 			break;
 		case 1:
 			SubPeliculasUD09 peli2 = new SubPeliculasUD09("Capitán América: Un nuevo mundo", 12.99, 118, 12,
@@ -27,6 +27,7 @@ public class Tarea06UD09App {
 			JOptionPane.showMessageDialog(null, "Has seleccionado: " + "Capitán América: Un nuevo mundo.\n Dura "
 					+ peli2.getDuracionPeli() + " minutos y es para mayores de " + peli2.getEdadMinima() + " años.");
 			peli2.comprarEntrada(peli2.getEdadMinima(), peli2.precioEntrada);
+			peliSelec = peli2;
 			break;
 		case 2:
 			SubPeliculasUD09 peli3 = new SubPeliculasUD09("Misión: Imposible 8", 12.99, 163, 12,
@@ -34,23 +35,24 @@ public class Tarea06UD09App {
 			JOptionPane.showMessageDialog(null, "Has seleccionado: " + "Misión: Imposible 8.\n Dura "
 					+ peli3.getDuracionPeli() + " minutos y es para mayores de " + peli3.getEdadMinima() + " años.");
 			peli3.comprarEntrada(peli3.getEdadMinima(), peli3.precioEntrada);
+			peliSelec = peli3;
 			break;
 		default:
 			JOptionPane.showMessageDialog(null, "Nos has seleccionado una pelicula");
 			return;
 		}
-		//if (peliSelec != null) {
+		if (peliSelec != null) {
 
-		//	SubEspectadorUD09 usuario = new SubEspectadorUD09(null);
+			SubEspectadorUD09 usuario = new SubEspectadorUD09(null);
 
-			//int cantidadBoletos = peliSelec.comprarEntrada(peliSelec.getEdadMinima(), peliSelec.getPrecioEntrada());
-		//	if (cantidadBoletos > 0) {
-			//	SuperClassCine cine = new SuperClassCine();
-			//	cine.elegirAsiento(usuario, cantidadBoletos);
-			//}
-			//JOptionPane.showInternalMessageDialog(null, "Gracias por su compra\n" + "Que disfrute su pelicula");
-			// SuperClassCine cine = new SuperClassCine(pelicula, precioEntrada);
-	//	}
+			int cantidadBoletos = peliSelec.comprarEntrada(peliSelec.getEdadMinima(), peliSelec.getPrecioEntrada());
+			if (cantidadBoletos > 0) {
+				SuperClassCine cine = new SuperClassCine();
+				cine.elegirAsiento(usuario, cantidadBoletos);
+			}
+			JOptionPane.showInternalMessageDialog(null, "Gracias por su compra\n" + "Que disfrute su pelicula");
+		//	SuperClassCine cine = new SuperClassCine();
+		}
 
 	}
 }
