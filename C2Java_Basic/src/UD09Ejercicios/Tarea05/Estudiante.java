f9f848a1f560967136f6950cc98ace71826c2987
AniBnP <NIGHJHUREY17@gmail.com>
2025-04-30 13:42:40 +0200

    Cambio atributos y metodos

--- C2Java_Basic/src/UD09Ejercicios/Tarea05/Estudiante.java
+++ C2Java_Basic/src/UD09Ejercicios/Tarea05/Estudiante.java
@@ -6,6 +6,26 @@ public class Estudiante {
 	protected String nombre;
 	protected int edad;
 	protected char sexo;
-	protected int calificacion;
-	protected boolean asistencia;
+	protected double calificacion;
+//	protected boolean asistencia;
+
+	// Constructor
+	public Estudiante(String nombre, int edad, char sexo, int calificacion) {
+		this.nombre = nombre;
+		this.edad = edad;
+		this.sexo = sexo;
+		this.calificacion = calificacion;
+	}
+
+	public boolean haceNovillos() {
+		return Math.random() > 0.5;
+	}
+
+	public double getCalificacion() {
+		return calificacion;
+	}
+
+	public char getSexo() {
+		return sexo;
+	}
 }
