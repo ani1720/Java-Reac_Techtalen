7e7ae91bbad5caf7e132f8b6105e830861161d53
AniBnP <NIGHJHUREY17@gmail.com>
2025-04-30 13:42:27 +0200

    Cambio atributos y metodos

--- C2Java_Basic/src/UD09Ejercicios/Tarea05/Profesor.java
+++ C2Java_Basic/src/UD09Ejercicios/Tarea05/Profesor.java
@@ -6,4 +6,22 @@ public class Profesor {
 	protected int edad;
 	protected char sexo;
 	protected String materia;
+	
+	//Contructor
+	public Profesor(String nombre, int edad, char sexo, String materia) {
+		this.nombre = nombre;
+		this.edad = edad;
+		this.sexo = sexo;
+		this.materia = materia;
+	}
+	public Profesor(String nombre, String materia) {
+		this.nombre = nombre;
+		this.materia = materia;
+	}
+	public boolean estaDisponible() {
+		return Math.random() > 0.2;
+	}
+	public String getMateria() {
+		return materia;
+	}
 }
