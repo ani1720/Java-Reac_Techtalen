9daaf29cbcbbc700e8ae9effabfff9db447952da
AniBnP <NIGHJHUREY17@gmail.com>
2025-05-15 10:56:07 +0200

    Tareas BBDD unidad 14

---
+++ C3BasesDatos/UD14/EJ08Directores.sql
@@ -0,0 +1,28 @@
+CREATE DATABASE directores_despachos2025;
+USE directores_despachos2025;
+CREATE TABLE despachos 
+    (num INT,
+    --AUTO_INCREMENT
+    capacidad INT NOT NULL,
+    PRIMARY KEY (num));
+
+CREATE TABLE directores
+    (DNI VARCHAR(9),
+    NomApels VARCHAR(255),
+    DNIJefe VARCHAR(9),
+    despacho INT NOT NULL,
+    PRIMARY KEY (DNI),
+    FOREIGN KEY (DNIJefe) 
+    REFERENCES directores(DNI)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+    FOREIGN KEY (despacho) 
+    REFERENCES despachos (num)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE);
+
+-- Le añadimos AUTO_INCREMENT
+ALTER TABLE despachos CHANGE num num INT(11) NOT NULL AUTO_INCREMENT;
+
+-- Rectificamos el valor NOT NULL de DNIjefe
+ALTER TABLE `directores` CHANGE `DNIJefe` `DNIJefe` VARCHAR(9) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL;
