977d4dd63328b9d2acecba3f622a792cec215b6a
AniBnP <NIGHJHUREY17@gmail.com>
2025-05-15 11:00:18 +0200

    Ejercicios Practicos Unidad 16

---
+++ C3BasesDatos/UD17MONGODB/Directores.js
@@ -0,0 +1,4 @@
+db.directores.insertMany([
+  { "DNI": "3333", "NomApels": "Garcia", "DNIJefe": null, "despacho": "2" },
+  { "DNI": "2345", "NomApels": "Esteve", "DNIJefe": null, "despacho": "4" }
+])
