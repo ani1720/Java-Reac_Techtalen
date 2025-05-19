8570ccf5944a19c6aeebe13a7b2e32f8a3ad9feb
AniBnP <NIGHJHUREY17@gmail.com>
2025-05-19 13:51:38 +0200

    Proyecto Grupal

---
+++ C2Java_Basic/src/UD19SwingAwt/CaculadoraApp.java
@@ -0,0 +1,10 @@
+package UD19SwingAwt;
+
+public class CaculadoraApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ C2Java_Basic/src/UD19SwingAwt/Calculadora.java
@@ -0,0 +1,31 @@
+package UD19SwingAwt;
+
+import java.awt.EventQueue;
+import java.sql.Connection;
+
+import javax.swing.*;
+//import javax.awt.*;
+
+public class Calculadora extends JFrame {
+
+	private JTextField txtOperando1, txtOperando2, txtResultado;
+    private JButton btnSuma, btnResta, btnMultiplicacion, btnDivision, btnPunto, btnIgual;
+    private JLabel lblResultado, lblTitulo;
+    private Connection conexion;
+    
+   public void calculadora2 () {
+	   setTitle("La Calculator");
+	   setBounds(400,200,600,500);
+	   setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setVisible(true);
+		
+//		JTextField txtOperando1 = new JTJTextField();
+//		txtOperando1.setBounds();
+//		JTextField txtOperando2 = new JTJTextField();
+//		JTextField txtResultado = new JTJTextField();
+		
+   }
+   public static void main (String[] args) {
+	   calculadora2();
+   }
+}
