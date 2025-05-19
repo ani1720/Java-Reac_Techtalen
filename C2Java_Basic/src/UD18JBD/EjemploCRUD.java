9636f8161a8a992ce6522beccb3306c5cffb788b
AniBnP <NIGHJHUREY17@gmail.com>
2025-05-19 13:51:26 +0200

    Actualizacion explicacion

--- C2Java_Basic/src/UD18JBD/EjemploCRUD.java
+++ C2Java_Basic/src/UD18JBD/EjemploCRUD.java
@@ -5,7 +5,7 @@ import java.sql.*;
 public class EjemploCRUD {
 
 	// Datos de conexión a la base de datos
-	static final String URL = "jdbc:mysql://localhost:3306/tiendainfo";
+	static final String URL = "jdbc:mysql://localhost:3306/calculadora";
 	static final String USER = "root";
 	static final String PASSWORD = "";
 
@@ -22,23 +22,24 @@ public class EjemploCRUD {
 			statement = conexion.createStatement();
 
 			// Insertar 3 registros en la tabla 'fabricantes'
-			statement.executeUpdate("INSERT INTO fabricantes (nombre) VALUES ('Sony')");
-			statement.executeUpdate("INSERT INTO fabricantes (nombre) VALUES ('Apple')");
-			statement.executeUpdate("INSERT INTO fabricantes (nombre) VALUES ('Samsung')");
+			statement.executeUpdate("INSERT INTO `historial` (`ID`, "
+					+ "`Operador1`, `Operador2`, `Operacion`, `Resultado`, "
+					+ "`Fecha`) VALUES (NULL, '5', '6', '+', '11', current_timestamp());");
+			
 
 			// Actualizar el nombre de un registro en la tabla 'fabricantes'
-			statement.executeUpdate("UPDATE fabricantes SET nombre = 'Microsoft' WHERE codigo = 2");
+//			statement.executeUpdate("UPDATE fabricantes SET nombre = 'Microsoft' WHERE codigo = 2");
 
 			// Eliminar un registro específico de la tabla 'fabricantes'
-			statement.executeUpdate("DELETE FROM fabricantes WHERE codigo = 3");
+//			statement.executeUpdate("DELETE FROM fabricantes WHERE codigo = 3");
 
 			// Consultar y mostrar los registros de la tabla 'fabricantes'
-			ResultSet resultSet = statement.executeQuery("SELECT * FROM fabricantes");
-			System.out.println("Registros en la tabla 'fabricantes':");
-			while (resultSet.next()) {
-				System.out.println(
-						"Código: " + resultSet.getInt("codigo") + ", Nombre: " + resultSet.getString("nombre"));
-			}
+//			ResultSet resultSet = statement.executeQuery("SELECT * FROM fabricantes");
+//			System.out.println("Registros en la tabla 'fabricantes':");
+//			while (resultSet.next()) {
+//				System.out.println(
+//						"Código: " + resultSet.getInt("codigo") + ", Nombre: " + resultSet.getString("nombre"));
+//			}
 
 		} catch (SQLException e) {
 			System.out.println("Error al conectar a la base de datos: " + e.getMessage());
---
+++ C2Java_Basic/src/UD19SwingAwt/AplicacionGraficaApp.java
@@ -0,0 +1,10 @@
+package UD19SwingAwt;
+
+public class AplicacionGraficaApp {
+
+	public static void main(String[] args) {
+	
+				AplicacionGrafica ventana =  new AplicacionGrafica();
+			}
+}
+	
