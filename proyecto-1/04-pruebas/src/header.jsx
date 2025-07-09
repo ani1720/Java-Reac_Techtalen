0ca54beb45afdffc993b392bf0ed4a3ef49249be
AniBnP <NIGHJHUREY17@gmail.com>
2025-07-09 09:22:37 +0200

    header

--- proyecto-1/04-pruebas/src/header.jsx
+++ proyecto-1/04-pruebas/src/header.jsx
@@ -1,13 +1,15 @@
 import Logo from "./assets/UppAllNight.png"
+import './header.css'
 const Header = () => {
     return (
         <>
+        <div className="logo"><img src={Logo} alt="Imagen de prueba" /></div>
         <h1>Welcome to Comunity</h1>
         <nav>
             <a href="Prueba.jsx">Prueba 1</a>
             <a href="Prueba2.jsx">Prueba2</a>
         </nav>
-        <img src={Logo} alt="Imagen de prueba" />
+        
         </>
     )
 }
